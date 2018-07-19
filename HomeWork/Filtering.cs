777d671e67f9cae766552158a04034d04744d24a
ausrineju <ausrine452@gmail.com>
2018-07-19 20:33:12 +0300

    Changes

---
+++ HomeWork/Filtering.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    class Filtering : IFilterICanFilterCollections
+    {
+        public Dictionary<int, string> FilterDictionaryKeys(Dictionary<int, string> data, Func<int, bool> filterLogic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, string> FilterDictionaryValues(Dictionary<int, string> data, Func<string, bool> filterLogic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> FilterIntegers(IEnumerable<int> data, Func<int, bool> filterLogic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> FilterStrings(IEnumerable<string> data, Func<string, bool> filterLogic)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> FilterStrings(IEnumerable<string> data, IEnumerable<string> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+   
+    }
+}
--- HomeWork/HomeWork.cs
+++ HomeWork/HomeWork.cs
@@ -8,8 +8,20 @@ namespace HomeWork
 {
     class HomeWork
     {
+        public static void RandomNumbers()
+        {
+            Random num = new Random();
+            Console.WriteLine("Random numbers from 1 to 10");
+            for (int i=0; i<100; i++)
+            {
+                 Console.Write(num.Next(1, 10) + " " );
+            }
+        }
+
         static void Main(string[] args)
         {
+            RandomNumbers();
+            Console.ReadKey();
         }
     }
 }
